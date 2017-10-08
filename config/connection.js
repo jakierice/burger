5535dc8f5f84f98e650fa9f80da15c3cf5a0a5e1
Jakie Rice <jakie.rice@gmail.com>
2017-10-08 14:13:06 -0500

    Add config for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,21 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-
 const keys = require('./keys');
 
-const connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	// user: 'root',
-	// password: 'evaunit2',
-	user: keys.user,
-	password: keys.password,
-	database: "flavors_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "localhost",
+		user: keys.user,
+		password: keys.password,
+		database: "flavors_db"
+	});
+}
+
 
 // Make connection.
 connection.connect(function (err) {
